77765a60ab8c86a77098af0539914ad9d103d6fb
Deen Abdullah <phakhruddin@gmail.com>
2024-07-16 06:30:40 +0800

    Initial Commit

---
+++ FirstShot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirstShot
+//
+//  Created by wannoorbaya wanchik on 7/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirstShot/FirstShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstShotApp.swift
+//  FirstShot
+//
+//  Created by wannoorbaya wanchik on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
